3daa042d1a743c08aaf549c6d26c5b359ccda4de
ThomasRgbg <ThomasRgbg@users.noreply.github.com>
2023-11-05 19:28:36 +0100

    Be more or less verbose with output

--- fronius_influxdb.py
+++ fronius_influxdb.py
@@ -11,19 +11,19 @@ from config_data import *
 
 if __name__ == "__main__":
     import time
-    
+
     argparser = argparse.ArgumentParser()
-    
-    argparser.add_argument("-a", "--address", help="IP Address of Symo", 
+
+    argparser.add_argument("-a", "--address", help="IP Address of Symo",
                            action='store')
-    argparser.add_argument("-t", "--database-table", help="Name of Database Table", 
+    argparser.add_argument("-v", "--verbose", help="Be more verbose",
+                           action='store_true')
+    argparser.add_argument("-t", "--database-table", help="Name of Database Table",
                            action='store')
-    argparser.add_argument("-c", "--config", help="Use predefined config (starts with 0)", 
+    argparser.add_argument("-c", "--config", help="Use predefined config (starts with 0)",
                            action='store')
     args = argparser.parse_args()
-    
-    print(args)
-    
+
     if args.config and args.address:
         print("Can not use predefined config and setting address via commandline at some time") 
         sys.exit(1)
@@ -41,8 +41,8 @@ if __name__ == "__main__":
     print("Using IP {0}, Database Table {1}".format(ipaddr, influxdb_table))
 
     influxdb = influxdb_cli2(influxdb_url, influxdb_token, org=influxdb_org, bucket=influxdb_bucket)
-    influxdb_table = 'pv_fronius'    
-    
+    influxdb_table = 'pv_fronius'
+
     symo = Symo(ipaddr)
     print("Found Inverter {0}".format(symo.name))
 
@@ -51,24 +51,26 @@ if __name__ == "__main__":
     calculated_parameters = symo.get_all_calculated()
 
     while True:
-        
+
         for name in parameters:
             if parameter_ignore in name:
                 continue
             value = symo.read_data(name)
-            
-            print('{0} = {1}'.format(name,value))
-            
+
+            if args.verbose:
+                print('{0} = {1}'.format(name,value))
+
             influxdb.write_sensordata(influxdb_table, name, value)
             time.sleep(0.02)
-            
+
         for name in calculated_parameters:
             if parameter_ignore in name:
                 continue
             value = symo.read_calculated_value(name)
-            
-            print('{0} = {1}'.format(name,value))
-            
+
+            if args.verbose:
+                print('{0} = {1}'.format(name,value))
+
             influxdb.write_sensordata(influxdb_table, name, value)
             time.sleep(0.02)
 
